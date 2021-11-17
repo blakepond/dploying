42104496f29ed42a4aad73d77bf00ccaa8a645e1
Blake Pond <blakehp@icloud.com>
2021-11-17 13:50:42 -0700

    first deploy

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const path = require('path')
 
 const app = express()
 
-app.get('/',function (req, res){
+app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+  })
 
 const port = process.env.PORT || 4005
 
